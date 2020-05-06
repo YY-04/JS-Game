92036a8befa0fcdd983e719313ca1c1014fb4f50
YY-04 <61716604+YY-04@users.noreply.github.com>
2020-05-06 14:09:03 +0900

    Update sketch.js

--- Javascript/project-1/sketch.js
+++ Javascript/project-1/sketch.js
@@ -3,7 +3,7 @@
 
 function setup() {
   createCanvas(1000, 500);
-  colorMode(HSL, 360, 100, 100, 100);
+  colorMode(HSL, 360, 100, 100, 100);  //HSL = 色相(Hue: 0~360) 彩度(Saturation: 0~100%) 明度(Lightness: 0~ 100%)
   background("#ffffff");
 }
 
